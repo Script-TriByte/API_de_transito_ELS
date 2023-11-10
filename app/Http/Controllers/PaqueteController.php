656e3b9a799fef8aa13e3d94bc59d7584a5b81c2
Maykol Martinez <MJoelMartinez320@gmail.com>
2023-11-10 12:05:52 -0300

    Inicio de separacion de consultas

--- app/Http/Controllers/PaqueteController.php
+++ app/Http/Controllers/PaqueteController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 
 use App\Models\PaqueteLote;
+use App\Models\Lote;
 
 class PaqueteController extends Controller
 {
@@ -16,21 +17,65 @@ class PaqueteController extends Controller
         DB::raw('LOCK TABLE paquete_lote READ');
     }
 
-    public function ListarTodos()
+    public function IniciarTransaccion()
     {
         $this->BloquearTablasASoloLectura();
         DB::beginTransaction();
+    }
+
+    public function FinalizarTransaccion()
+    {
+        DB::commit();
+        DB::raw('UNLOCK TABLES');
+    }
+
+    public function ListarTodos()
+    {
+        $this->IniciarTransaccion();
 
         $paquetes = PaqueteLote::join('paquetes', 'paquete_lote.idPaquete', '=', 'paquetes.idPaquete')
         ->select('paquetes.*')
         ->get();
 
-        DB::commit();
-        DB::raw('UNLOCK TABLES');
+        $this->FinalizarTransaccion();
 
         if($paquetes == null)
             return ["mensaje" => "No hay paquetes en envio."];
 
         return json_encode($paquetes);
+    }
+
+    public function CalcularTiempoDeLlegada(Request $request, $idPaquete)
+    {
+        $informacionDeLote = $this->ObtenerInformacionDeLote();
+        $idLote = $informacionDeLote->idLote;
+        $loteSiendoTransportado = VehiculoLoteDestino::findOfFail($idLote);
+
+        $horaEstimadaDeLlegada = $loteSiendoTransportado->horaEstimada;
+
+        
+    }
+
+    public function ObtenerDestinoAsignado(Request $request, $idPaquete)
+    {
+        $informacionDeLote = $this->ObtenerInformacionDeLote();
+        $destinoDeLote = $informacionDeLote->idDestino;
+        $informacionDestino = Destino::where('idDestino', $destinoDeLote)->first();
+
+        return $informacionDestino->nombre; 
+    }
+
+    public function ObtenerInformacionDeLote($idPaquete)
+    {
+        $loteRelacionado = PaqueteLote::findOrFail($idPaquete);
+        $idLote = $loteRelacionado->idLote;
+        $informacionDeLote = Lote::where('idLote', $idLote)->first();
+
+        return $informacionDeLote;
+    }
+
+    public function ObtenerInformacionDeArticulo(Request $request, $idPaquete)
+    {
+
     }
 }
