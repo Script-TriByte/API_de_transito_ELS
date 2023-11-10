b90a4a5872374ce4d92831943fcc13841a443df7
Diego Javier <diegoa.javierg2501@gmail.com>
2023-11-10 20:54:21 -0300

    Inicio de Try-Catch en LoteController

--- app/Http/Controllers/LoteController.php
+++ app/Http/Controllers/LoteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\QueryException;
 
 use App\Models\Lote;
 use App\Models\EstadoEntrega;
@@ -30,33 +31,46 @@ class LoteController extends Controller
 
     public function CrearEstadoEntrega($idLote)
     {
-        $fechaDeEntrega = date('Y-m-d');
-        $horaDeEntrega = date('H:i:s');
+        try {
+            $fechaDeEntrega = date('Y-m-d');
+            $horaDeEntrega = date('H:i:s');
 
-        EstadoEntrega::create([
-            'idLote' => $idLote,
-            'fechaEntrega' => $fechaDeEntrega,
-            'horaEntrega' => $horaDeEntrega
-        ]);
+            EstadoEntrega::create([
+                'idLote' => $idLote,
+                'fechaEntrega' => $fechaDeEntrega,
+                'horaEntrega' => $horaDeEntrega
+            ]);
+        }
+        catch (QueryException $e) {
+            return [ "mensaje" => "No se ha podido conectar a la base de datos. Intentelo mas tarde." ];
+        }
     }
 
     public function ConfirmarEntrega(Request $request, $idLote)
     {
-        $validation = Validator::make(['idLote' => $idLote],[
-            'idLote' => 'required|numeric',
-        ]);
-
-        if($validation->fails())
-            return response($validation->errors(), 401);
-
-        Lote::findOrFail($idLote);
-
-        $this->IniciarTransaccion();
-
-        $this->CrearEstadoEntrega($idLote);
-
-        $this->FinalizarTransaccion();
-
-        return [ "mensaje" => "Se ha entregado el lote correctamente."];
+        try {
+            $validation = Validator::make(['idLote' => $idLote],[
+                'idLote' => 'required|numeric',
+            ]);
+    
+            if($validation->fails())
+                return response($validation->errors(), 401);
+    
+            VehiculoLoteDestino::findOrFail($idLote);
+    
+            $this->IniciarTransaccion();
+    
+            $this->CrearEstadoEntrega($idLote);
+    
+            $this->FinalizarTransaccion();
+    
+            return [ "mensaje" => "Se ha entregado el lote correctamente."];
+        }
+        catch (QueryException $e) {
+            return [ "mensaje" => "No se ha podido conectar a la base de datos, intentelo mas tarde." ];
+        }
+        catch (ModelNotFoundException $e){
+            return [ "mensaje" => "Lote inexistente." ];
+        }
     }
 }
