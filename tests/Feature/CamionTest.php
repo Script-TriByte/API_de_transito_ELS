9d1da2480cd10d69c957c2112ec164664dacf32f
Maykol Martinez <MJoelMartinez320@gmail.com>
2023-11-14 15:13:37 -0300

    Correccion testing camion

--- tests/Feature/CamionTest.php
+++ tests/Feature/CamionTest.php
@@ -11,28 +11,30 @@ class CamionTest extends TestCase
 {
     public function test_IntentarListarSinAutenticacion()
     {
-        $response = $this->get('/api/v3/contenidos/45000000');
+        $response = $this->get('/api/v3/contenidos/45000000', [
+            "Accept" => "application/json"
+        ]);;
         $response->assertStatus(403);
     }
 
     public function test_VerificarFuncionamientoDelListado()
     {
         $user = User::first();
-        $response = $this->actingAs($user)->get('/api/v3/contenidos/45000000');
+        $response = $this->actingAs($user, "api")->get('/api/v3/contenidos/45000000');
         $response->assertStatus(200);
     }
 
     public function test_ListarContenidosDeUnCamionSinDatos()
     {
         $user = User::first();
-        $response = $this->actingAs($user)->get('/api/v3/contenidos/{}');
+        $response = $this->actingAs($user, "api")->get('/api/v3/contenidos/{}');
         $response->assertStatus(400);
     }
 
     public function test_ListarContenidosDeUnChoferInexistente()
     {
         $user = User::first();
-        $response = $this->actingAs($user)->get('/api/v3/contenidos/99999999');
+        $response = $this->actingAs($user, "api")->get('/api/v3/contenidos/99999999');
         $response->assertStatus(200);
         $this->assertEquals([], $response);
     }
