3d3a37d738dec798aabc41e23d1ba712aa858e58
Maykol Martinez <MJoelMartinez320@gmail.com>
2023-11-14 01:33:17 -0300

    Segunda correccion de sintaxis

--- database/factories/UsuarioFactory.php
+++ database/factories/UsuarioFactory.php
@@ -18,7 +18,7 @@ class UsuarioFactory extends Factory
             'nombre' => $this->faker->name(),
             'apellido' => $this->faker->lastname(),
             'telefono' => $this->faker->numberBetween(91000000, 97999999),
-            'direccion' => $this->substr(faker->address, 1, 40),
+            'direccion' => $this->substr(faker->address(), 1, 40),
         ];
     }
 }
