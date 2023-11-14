5573e1f64920b93c9660b41de4ae59a81343b8d2
Maykol Martinez <MJoelMartinez320@gmail.com>
2023-11-14 13:18:03 -0300

    Implementacion de informacion faltante en modelo

--- app/Models/Chofer.php
+++ app/Models/Chofer.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Chofer extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'docDeIdentidad';
+
+    protected $table = 'choferes';
+
+    protected $fillable = [
+        'docDeIdentidad'
+    ];
 }
